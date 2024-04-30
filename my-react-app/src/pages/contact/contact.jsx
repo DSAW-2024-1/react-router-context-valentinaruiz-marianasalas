52e0cd68288c268879ab150162f416d05b86d8b3
MariSalas23 <mari.masagu@gmail.com>
2024-04-29 20:14:45 -0500

    Update Components Home and Contact

---
+++ my-react-app/src/pages/contact/contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Feel free to contact us!</p>
+      <p>We are here to help you and answer your questions:</p>
+      <ul>
+        <li>304 2056621</li>
+        <li>marianasalgu@unisabana.edu.co</li>
+        <li>valentinaruito@unisabana.edu.co</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
